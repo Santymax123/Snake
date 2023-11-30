bce35100067dad744e5b70946d4506c8b633ac50
Max Stoner <83330047+Santymax123@users.noreply.github.com>
2023-11-30 15:40:15 +0800

    Update Square.py
    added all snake squares for rendering.

--- Snake/Square.py
+++ Snake/Square.py
@@ -8,7 +8,23 @@ class squareType(Enum):
     EMPTY = 0
     WALL = 1
     FOOD = 2
+    # Remove eventually
     SNAKE = 3
+    # To be used for head. the OPPOSITE can also be used for tail 
+    UP = 4
+    DOWN = 5
+    LEFT = 6
+    RIGHT = 7
+    # For anything between head and tail - each multi-direction one can be used for travelling both directions (see Snake Rendering.PNG
+    BODY_VERTICAL = 8
+    BODY_HORIZONTAL = 9
+    BODY_UR = 10
+    BODY_DR = 11
+    BODY_DL = 12
+    BODY_UL = 13
+    
+    
+    
 
 class Square:
     def __init__(self, x, y):
